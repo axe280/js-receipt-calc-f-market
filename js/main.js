88968acb82ea99b8488331f76de04e1133a54de9
axe280 <axeworkmail@gmail.com>
2020-10-02 18:21:35 +0300

    app 0.0.1

---
+++ js/main.js
@@ -0,0 +1,161 @@
+(function(){
+  const $usersElems = document.querySelectorAll('[data-user]'),
+        $percentElems = document.querySelectorAll('[data-percent]'),
+        $userTitle = document.querySelector('[data-user-title]'),
+        $inputTitle = document.querySelector('[data-input-title]'),
+        $input = document.querySelector('[data-input]'),
+        $arrow = document.querySelector('[data-arrow]'),
+        $transactionValue = document.querySelector('[data-transaction-value]'),
+        $calcBtn = document.querySelector('[data-calc-btn]');
+
+  const numberPattern = /^\d+$/;
+
+  const errorMessages = {
+    errorMsgUser: 'Choose one of the cats',
+    errorMsgInput: 'How much money did you spend?',
+  }
+
+  let state = {
+    activeUser: '0',
+    activePercent: null,
+    inputValue: 0,
+    richestPerson: '0',
+    transactionValue: 0,
+    defaultUserText: '',
+    usersSum: [0, 0],
+    defaultPercentText: 'Your investment',
+    defaultInputText: 'Give me your money'
+  };
+
+  $userTitle.textContent = state.defaultUserText;
+  $inputTitle.textContent = state.defaultInputText;
+
+  // user click handler
+  $usersElems.forEach($user => {
+    $user.addEventListener('click', function(event) {
+      const $userBtn = this;
+      state.activeUser = $userBtn.dataset.user;
+      $userTitle.textContent = state.defaultUserText;
+
+      if (state.activeUser === '1') {
+        state.activePercent = 67;
+      }
+
+      if (state.activeUser === '2') {
+        state.activePercent = 33;
+      }
+
+      $usersElems.forEach(elem => {
+        elem.classList.remove('active');
+      });
+
+      $percentElems.forEach(elem => {
+        elem.classList.remove('active');
+      });
+
+      const percentBtn = findActivePercentEl(state.activePercent);
+      percentBtn.classList.add('active');
+      $userBtn.classList.add('active');
+    });
+  });
+
+  // percent click handler
+  $percentElems.forEach($percentBtn => {
+    $percentBtn.addEventListener('click', function(event) {
+      $percentElems.forEach(elem => {
+        elem.classList.remove('active');
+      });
+
+      this.classList.add('active');
+      state.activePercent = Number(this.dataset.percent);
+    });
+  });
+
+  // input handler
+  $input.addEventListener('input', function(event) {
+    if (event.data === '.' || !numberPattern.test(Number(this.value))) {
+      this.value = this.value.slice(0, -1);
+      return;
+    }
+
+    $inputTitle.textContent = state.defaultInputText;
+    state.inputValue = Number(this.value);
+  });
+
+  // carc button handler
+  $calcBtn.addEventListener('click', function(evetn) {
+    if (state.activeUser === '0') {
+      $userTitle.textContent = errorMessages.errorMsgUser;
+      return;
+    }
+
+    if (state.inputValue <= 0 ) {
+      $inputTitle.textContent = errorMessages.errorMsgInput;
+      return;
+    }
+
+    calculateApp();
+  });
+
+  const findActivePercentEl = (percent) => {
+    return Array.from($percentElems).find((percentBtn) => {
+      return Number(percentBtn.dataset.percent) === percent;
+    });
+  }
+
+  const calculateApp = () => {
+    const sum = state.inputValue - Math.round((state.inputValue * state.activePercent / 100));
+
+    if (
+      (state.richestPerson === state.activeUser)
+      || (state.richestPerson === "0")
+    ) {
+      state.transactionValue += sum;
+      state.richestPerson = state.activeUser;
+    }
+
+    if (
+      (state.richestPerson === "1" && state.activeUser === "2")
+      || (state.richestPerson === "2" && state.activeUser === "1")
+    ) {
+      state.transactionValue -= sum;
+
+      if (state.transactionValue < 0) {
+        state.transactionValue = Math.abs(state.transactionValue);
+        state.richestPerson = state.activeUser;
+      }
+    }
+
+    if (state.richestPerson === "1") {
+      state.usersSum[0] += state.inputValue - sum;
+      state.usersSum[1] += sum;
+    }
+
+    if (state.richestPerson === "2") {
+      state.usersSum[0] += sum;
+      state.usersSum[1] += state.inputValue - sum;
+    }
+
+    changeArrowDirection();
+    setSumValues();
+    $transactionValue.textContent = state.transactionValue;
+  };
+
+  const changeArrowDirection = () => {
+    if (state.richestPerson === '1') {
+      $arrow.classList.add('reverse');
+    } else {
+      $arrow.classList.remove('reverse');
+    }
+  };
+
+  const setSumValues = () => {
+    $usersElems[0].querySelector('[data-user-value]').textContent = state.usersSum[0];
+    $usersElems[1].querySelector('[data-user-value]').textContent = state.usersSum[1];
+  };
+
+  calculateApp();
+
+}());
+
+
